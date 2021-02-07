72e70c42efee462d6a747cd3ce66d44f91cfd642
James_C <a9356070@gmail.com>
2021-02-07 18:07:36 +0800

    refactor express router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const passport = require('passport')
 const usePassport = require('./config/passport')
+const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
@@ -24,64 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 usePassport(app)
 
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
-})
-
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/users/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
-
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-app.post('/users/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  User.findOne({ where: { email } })
-    .then(user => {
-      if (user) {
-        return res.render('register', {
-          name,
-          email,
-          password,
-          confirmPassword
-        })
-      }
-      return bcrypt
-        .genSalt(10)
-        .then(salt => bcrypt.hash(password, salt))
-        .then(hash => User.create({
-          name,
-          email,
-          password: hash
-        }))
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    })
-})
-
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
-
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
