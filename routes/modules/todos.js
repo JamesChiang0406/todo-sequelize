6bbc3959e42689482c3605d0daa5c0e52e788dde
James_C <a9356070@gmail.com>
2021-02-08 15:27:48 +0800

    add create function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,9 +3,23 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
+
   return Todo.findOne({
     where: {
       id,
